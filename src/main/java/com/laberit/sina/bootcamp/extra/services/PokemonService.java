baac6d0489000e59da9302ee858bce8f9731fd53
jrubio <jrubio@laberit.com>
2024-09-26 11:56:54 +0200

    Reformat and delete unused imports

--- src/main/java/com/laberit/sina/bootcamp/extra/services/PokemonService.java
+++ src/main/java/com/laberit/sina/bootcamp/extra/services/PokemonService.java
@@ -1,11 +1,9 @@
 package com.laberit.sina.bootcamp.extra.services;
 
 import com.laberit.sina.bootcamp.extra.models.Pokemon;
-import com.laberit.sina.bootcamp.extra.models.PokemonNames;
-
-import java.util.List;
 
 public interface PokemonService {
     Pokemon getPokemon(String name);
+
     Pokemon getPokemon(int id);
 }
--- src/main/java/com/laberit/sina/bootcamp/extra/services/PokemonServiceImpl.java
+++ src/main/java/com/laberit/sina/bootcamp/extra/services/PokemonServiceImpl.java
@@ -2,25 +2,16 @@ package com.laberit.sina.bootcamp.extra.services;
 
 import com.laberit.sina.bootcamp.extra.api.PokeApiClient;
 import com.laberit.sina.bootcamp.extra.models.Pokemon;
-import com.laberit.sina.bootcamp.extra.models.PokemonNames;
-import com.laberit.sina.bootcamp.extra.models.PokemonNamesResponse;
-import com.laberit.sina.bootcamp.extra.models.PokemonNamesResult;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class PokemonServiceImpl implements PokemonService {
 
     @Override
     public Pokemon getPokemon(String name) {
         return PokeApiClient.getPokemon(name);
-
     }
 
     @Override
     public Pokemon getPokemon(int id) {
         return PokeApiClient.getPokemon(id);
-
     }
 }
